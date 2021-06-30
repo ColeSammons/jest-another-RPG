e200e72493f9bc4e021e0840ddf61d761406d5f2
Cole Sammons <colemsammons@gmail.com>
2021-06-30 13:08:08 -0700

    multiple methods created for palyer

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,5 +1,4 @@
 const Potion = require('../lib/Potion');
-const Player = require('../lib/Player')
 
 test('creates a health potion object', () => {
     const potion = new Potion('health');
@@ -14,23 +13,4 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
-  });
-
-  test("gets player's stats as an object", () => {
-    const player = new Player('Dave');
-  
-    expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('health');
-    expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility');
-  });
-
-  test('gets inventory from player or returns false', () => {
-    const player = new Player('Dave');
-  
-    expect(player.getInventory()).toEqual(expect.any(Array));
-  
-    player.inventory = [];
-  
-    expect(player.getInventory()).toEqual(false);
   });
